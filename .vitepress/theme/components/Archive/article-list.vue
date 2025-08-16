8729a8551b365aaa70bf8a67b7a5d9e366701f3a
荣轩浩 <brf9577@163.com>
2025-08-16 23:25:30 +0800

    特性: 完成归档页面

--- .vitepress/theme/components/Archive/article-list.vue
+++ .vitepress/theme/components/Archive/article-list.vue
@@ -6,24 +6,9 @@
         <span class='text-sm text-neutral-500'> - {{ list.length }}篇</span>
       </div>
     </div>
-    <div class='relative flex flex-wrap'>
-      <div v-for="(data, i) in list" :key='i' class='w-full md:w-1/2 xl:w-1/3 2xl:w-1/4 p-1'>
-        <div
-          class='rounded-2xl hover:scale-110 duration-500 p-4 bg-[var(--vp-c-bg)] shadow-xs shadow-[var(--vp-c-bg-reverse-rgb)]/5 dark:shadow-white/5 hover:shadow-xl hover:shadow-[var(--vp-c-bg-reverse-rgb)]/10 hover:dark:shadow-white'>
-          <a :href='data.url'>
-            <section class='space-y-2'>
-              <div class='relative font-bold text-md overflow-hidden line-clamp-1 whitespace-nowrap'
-                :title='data.frontmatter.title'>
-                {{ data.frontmatter.title }}
-              </div>
-              <div class='text-xs overflow-hidden line-clamp-2'>{{ data.frontmatter.description }}</div>
-              <div class="flex overflow-hidden gap-1" v-if="data.frontmatter.tags?.length">
-                <Badge type="tip" v-for="tag in data.frontmatter.tags" :key="tag" :text="tag" />
-              </div>
-              <div class='text-xs'>{{ FormatDate(data.frontmatter.LastEditTime, DATE_FORMAT) }}</div>
-            </section>
-          </a>
-        </div>
+    <div class='relative flex flex-wrap items-stretch'>
+      <div v-for="(data, i) in list" :key='i' class='min-h-full h-auto w-full md:w-1/2 xl:w-1/3 2xl:w-1/4 p-1'>
+        <ArticleComponent :data />
       </div>
     </div>
   </div>
@@ -31,6 +16,7 @@
 <script setup lang="ts">
 import { Article } from '../../common/article.data.mts';
 import { DATE_FORMAT, FormatDate } from '../../common/date.mts';
+import ArticleComponent from './article.vue';
 
 defineProps<{
   year: string;
---
+++ .vitepress/theme/components/Archive/mac-icons.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex flex-wrap absolute left-2 top-2 gap-1">
+    <span class="size-2 bg-[#ff605c] rounded-xl"></span>
+    <span class="size-2 bg-[#ffbd44] rounded-xl"></span>
+    <span class="size-2 bg-[#00ca4e] rounded-xl"></span>
+  </div>
+</template>
