2a25575db311be086f1bdd7fa3dc9d5a2847c0a4
rxht <brf9577@163.com>
2025-08-29 16:19:14 +0800

    文档: 新增汉字电码对照表

--- src/components/Symbols/index.vue
+++ src/components/Symbols/index.vue
@@ -2,9 +2,8 @@
   <div class="flex flex-col gap-4">
     <div v-if='!isSupported' v-html='notSupportedText' />
     <div class="flex flex-wrap gap-1">
-      <div v-for="(s, i) in SymbolsData" :key="`${i}_${s}`"
-        class="w-8 text-center font-bold rounded outline-1 outline-blue-300 hover:bg-blue-400 leading-[1.5]">
-        <button @click="copy(s)" class="w-full h-full">{{ s }}</button>
+      <div v-for="(s, i) in SymbolsData" :key="`${i}_${s}`" class="w-8 text-center font-bold rounded outline-1 outline-blue-300 hover:bg-blue-400 leading-[1.5]">
+        <button @click="handleCopy(s, i)" class="w-full h-full">{{ s }}</button>
       </div>
     </div>
   </div>
@@ -16,7 +15,12 @@ import { hookClipboard } from '../../common/hook';
 const { isSupported, notSupportedText, copy } = hookClipboard('字符');
 const props = defineProps<{
   symbols: string[] | string;
+  get?: (s: string, i: number) => string;
 }>();
 
 const SymbolsData = computed(() => Array.isArray(props.symbols) ? props.symbols : props.symbols.split('').filter(i => !!i));
+function handleCopy(s: string, i: number) {
+  if (props.get) return copy(props.get(s, i));
+  return copy(s);
+}
 </script>
